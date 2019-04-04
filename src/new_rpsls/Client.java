f8fb5d421729f8f4c50b06866238c2086d212608
Logan S <logan.scott441@gmail.com>
2019-04-04 13:44:45 -0500

    We now support creating a username and such on the clientside, still have to implment this server side

--- src/new_rpsls/Client.java
+++ src/new_rpsls/Client.java
@@ -1,9 +1,16 @@
 package new_rpsls;
 
 import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-import javax.xml.bind.Unmarshaller;
+import javafx.geometry.Insets;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
@@ -17,6 +24,54 @@ public class Client extends Application {
     //TODO:Implement this
     int port = 5555;
 
+    private void connectToServer(String ip, String port, String userName){
+        System.out.println("Ready to connect with this information");
+        System.out.println("IP: " + ip + ", Port: " + port + ", userName: " + userName);
+        try{
+            socket = new Socket("localhost",Integer.parseInt(port));
+            Listener temp = new Listener(
+                    new BufferedReader(new InputStreamReader(socket.getInputStream())),
+                    new PrintWriter(socket.getOutputStream())
+            );
+            temp.run();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    //Setup the gui for our first UI where we ask for a port, IP, and username
+    public Scene setupClientInfoUI(){
+        BorderPane clientPane = new BorderPane();
+        clientPane.setPadding(new Insets(5,5,5,5));
+        VBox container = new VBox();
+
+        //User name box
+        Label username = new Label("Username: ");
+        TextField userField=  new TextField("RPSGod");
+
+        //IP box
+        Label ip = new Label("IP Address");
+        TextField ipField = new TextField("192.0.0.1");
+
+        //Port box
+        Label port = new Label("Port:");
+        TextField portField = new TextField("5555");
+
+        //Finish button
+        Button submit = new Button("Connect");
+        submit.setOnAction(e->{
+            connectToServer(ipField.getText(),portField.getText(),username.getText());
+        });
+
+        //Add the vbox to the middle and add all buttons
+        clientPane.setCenter(container);
+        container.getChildren().addAll(username,userField,ip,ipField,port,portField,submit);
+
+        //Create a scene and return it
+        Scene newScene = new Scene(clientPane,700,500);
+        return newScene;
+    }
+
     public static void main(String[] args){
         launch(args);
     }
@@ -24,16 +79,8 @@ public class Client extends Application {
     //Implement the GUI
     @Override
     public void start(Stage primaryStage) throws Exception {
-        try{
-            socket = new Socket("localhost",port);
-            Listener temp = new Listener(
-                    new BufferedReader(new InputStreamReader(socket.getInputStream())),
-                    new PrintWriter(socket.getOutputStream())
-                    );
-            temp.run();
-        }catch(Exception e){
-            e.printStackTrace();
-        }
+        primaryStage.setScene(setupClientInfoUI());
+        primaryStage.show();
     }
 
     class Listener implements Runnable{
