0ae808fdff53e392f24fb0759a12b45fa264c775
DESKTOP-MLLUKQF\logan <logan.scott441@gmail.com>
2019-04-04 11:41:09 -0500

    Remade everything

--- src/DEPRECATED/Client.java
+++ src/DEPRECATED/Client.java
@@ -1,4 +1,4 @@
-package rps;
+package DEPRECATED;
 
 
 import java.io.*;
--- src/DEPRECATED/Server.java
+++ src/DEPRECATED/Server.java
@@ -1,4 +1,4 @@
-package rps;
+package DEPRECATED;
 
 import javafx.application.Application;
 import javafx.application.Platform;
